4944f607a665f77263c56f1dff61ddd1d5f17d6f
Alexandra Campo Matilla <acampoma@emeal.nttdata.com>
2025-07-10 09:20:11 +0200

    feature: add tasks in list

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+const [tasksList, setTasksList] = useState<string[]>([]);
+const [inputTaskValue, setInputTaskValue] = useState<string>("");
+type InputChange = React.ChangeEvent<HTMLInputElement>;
+type onSubmit = React.FormEvent<HTMLFormElement>;
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+console.log({tasksList})
+
+const handleWriteTask = (e: InputChange): void => {
+  setInputTaskValue(e.target.value);
 }
 
-export default App
+const handleAddTask = (e: onSubmit): void => {
+  e.preventDefault(); 
+  if(inputTaskValue.trim() === "") return; // No añadir tareas vacías
+  setTasksList( [...tasksList, inputTaskValue]);
+  setInputTaskValue(""); 
+}
+
+  return (
+    <div className="todo-container">
+      <h1>Lista de tareas</h1>
+      <form onSubmit={handleAddTask}>
+        <input type="text" placeholder="Añade una tarea" onChange={handleWriteTask} value={inputTaskValue}/>
+        <button type="submit">Añadir</button>
+      </form>
+      <ul>
+        {tasksList.map((task, index) => (
+          <li key={index}>
+            <input type="checkbox" />
+            <span>{task}</span>
+            <button onClick={() => setTasksList(tasksList.filter((_, i) => i !== index))}>
+              Eliminar
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+
